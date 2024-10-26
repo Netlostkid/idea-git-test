5c1fbd5333eb184e46b5a2adf20203a3d08723d1
max <maxtu6114@gmail.com>
2024-10-26 14:44:34 +0800

    更新了一个多行注释

--- src/main/java/com/itbaizhan/UserDao.java
+++ src/main/java/com/itbaizhan/UserDao.java
@@ -2,6 +2,9 @@ package com.itbaizhan;
 
 public class UserDao {
     //这个一个注释
+    /*
+    这是一个多行注释
+     */
     public static void main(String[] args) {
         System.out.println("hello idea git");
     }
